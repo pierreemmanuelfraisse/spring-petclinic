386f28808b4064d2e3bed2e5cf884b8a49190d5b
Amel Fraisse <6461848+amelfraisse@users.noreply.github.com>
2023-04-07 15:54:51 +0200

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -33,17 +33,18 @@ import javax.crypto.Cipher;
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		// try {
+		try {
 
-		Cipher c1 = Cipher.getInstance("DES");
+			Cipher c1 = Cipher.getInstance("DES");
 
-		int i = 0;
-		i++;
+			int i = 0;
+			i++;
 
-		SpringApplication.run(PetClinicApplication.class, args);
-		// } catch(Exception e) {
-		// e.printStackTrace();
-		// }
+			SpringApplication.run(PetClinicApplication.class, args);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
